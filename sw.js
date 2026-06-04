d7c0a19a1ec7772579e537feaba1b2795df8bbde
Kerwin <kerwin865218@gmail.com>
2026-06-04 23:17:01 +0800

    balance: side-by-side KRW/TWD, remove 甲乙 labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'busan-pwa-v35';
+const CACHE = 'busan-pwa-v36';
 const PRE = [
   './', './index.html', './manifest.json',
   './data/config.json', './data/flights.json', './data/hotels.json',
