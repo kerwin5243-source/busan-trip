2b76df0e8216e853755ca339db356251a54ffb41
Kerwin <kerwin865218@gmail.com>
2026-06-04 23:26:03 +0800

    balance: single col, KRW/TWD stacked per person, 萬 format, group totals

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'busan-pwa-v37';
+const CACHE = 'busan-pwa-v38';
 const PRE = [
   './', './index.html', './manifest.json',
   './data/config.json', './data/flights.json', './data/hotels.json',
