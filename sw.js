ed7173692db4ec5e0aa7d30311751753c6cad57c
Kerwin <kerwin865218@gmail.com>
2026-06-04 23:19:22 +0800

    balance: show names only in KRW column, remove avatar abbreviations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'busan-pwa-v36';
+const CACHE = 'busan-pwa-v37';
 const PRE = [
   './', './index.html', './manifest.json',
   './data/config.json', './data/flights.json', './data/hotels.json',
