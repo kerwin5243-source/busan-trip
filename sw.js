9d12da5b485866702c7efc92f6ef932a9c434c57
Kerwin <kerwin865218@gmail.com>
2026-06-04 23:09:31 +0800

    update banner subtitles and photo location labels for DAY2-5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'busan-pwa-v32';
+const CACHE = 'busan-pwa-v33';
 const PRE = [
   './', './index.html', './manifest.json',
   './data/config.json', './data/flights.json', './data/hotels.json',
