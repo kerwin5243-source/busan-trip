593281619d636a1948e2ae19faa64895d8fd25e5
Kerwin <kerwin865218@gmail.com>
2026-06-05 21:28:51 +0800

    fix Korean names, add bilingual menus, 札嘎其 seafood Korean, 述古堂 reserved

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'busan-pwa-v40';
+const CACHE = 'busan-pwa-v41';
 const PRE = [
   './', './index.html', './manifest.json',
   './data/config.json', './data/flights.json', './data/hotels.json',
